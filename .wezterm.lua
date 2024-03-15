45b1b6170e827899fee11eac0fc3de34f8591612
Jacob Luke Chrzanowski <jlc3467@rit.edu>
2024-03-15 00:14:20 -0400

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require 'wezterm'
+local act = wezterm.action
 local config = {}
 
 -- kitty key encoding
@@ -8,12 +9,16 @@ config.allow_win32_input_mode = true
 -- term
 config.term = 'xterm-256color'
 -- opacity
-config.window_background_opacity = 0
+--config.window_background_opacity = 0
 --config.text_background_opacity = 0.8
-config.win32_system_backdrop = 'Mica'
+--config.win32_system_backdrop = 'Mica'
 -- Style
 -- https://wezfurlong.org/wezterm/colorschemes/s/index.html#solarized-dark-terminalsexy
 config.color_scheme = 'Solarized (dark) (terminal.sexy)'
+config.color_scheme = 'OceanicNext (base16)'
+--config.color_scheme = 'Oceanic-Next'
+--config.color_scheme = 'OceanicMaterial'
+
 -- Font
 config.font_size = 10
 config.dpi = 96
@@ -21,6 +26,18 @@ config.font = wezterm.font(
     'Fira Code Regular',
     {}
 )
+-- X11
+config.enable_wayland = true
+
+-- Key Combos
+config.keys = {
+  -- paste from the clipboard
+  { key = 'V', mods = 'CTRL', action = act.PasteFrom 'Clipboard' },
+  -- paste from the primary selection
+  { key = 'V', mods = 'CTRL', action = act.PasteFrom 'PrimarySelection' },
+  -- CTRL-SHIFT-l activates the debug overlay
+  { key = 'L', mods = 'CTRL', action = wezterm.action.ShowDebugOverlay },
+}
 
 -- Window Frame
 config.window_frame = {
