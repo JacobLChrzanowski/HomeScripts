103badc2bf97cd30e794583841b08ad4db926813
Jacob Luke Chrzanowski <jlc3467@rit.edu>
2024-03-17 22:41:47 -0400

    Cleaned up ui in wezterm via config

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,5 +1,4 @@
 local wezterm = require 'wezterm'
-local act = wezterm.action
 local config = {}
 
 -- kitty key encoding
@@ -16,9 +15,8 @@ config.term = 'xterm-256color'
 -- https://wezfurlong.org/wezterm/colorschemes/s/index.html#solarized-dark-terminalsexy
 config.color_scheme = 'Solarized (dark) (terminal.sexy)'
 config.color_scheme = 'OceanicNext (base16)'
---config.color_scheme = 'Oceanic-Next'
---config.color_scheme = 'OceanicMaterial'
-
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
 -- Font
 config.font_size = 10
 config.dpi = 96
@@ -26,18 +24,6 @@ config.font = wezterm.font(
     'Fira Code Regular',
     {}
 )
--- X11
-config.enable_wayland = true
-
--- Key Combos
-config.keys = {
-  -- paste from the clipboard
-  { key = 'V', mods = 'CTRL', action = act.PasteFrom 'Clipboard' },
-  -- paste from the primary selection
-  { key = 'V', mods = 'CTRL', action = act.PasteFrom 'PrimarySelection' },
-  -- CTRL-SHIFT-l activates the debug overlay
-  { key = 'L', mods = 'CTRL', action = wezterm.action.ShowDebugOverlay },
-}
 
 -- Window Frame
 config.window_frame = {
